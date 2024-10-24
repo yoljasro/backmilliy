f70174c670658fa79456eda962c373e86331f905
yoljasro <saidaliyevjasur450@gmail.com>
2024-10-24 13:58:18 +0500

    changing

--- src/admin.router.js
+++ src/admin.router.js
@@ -1,43 +1,28 @@
 const { default: AdminBro } = require('admin-bro');
-const { buildAuthenticatedRouter } = require('admin-bro-expressjs');
+const { buildRouter } = require('admin-bro-expressjs');
 const express = require('express');
-const argon2 = require('argon2');
-const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-// models
-const { Company } = require('./companies/company.entity');
+const mongoose = require('mongoose');
 
 /**
  * @param {AdminBro} admin
  * @return {express.Router} router
  */
 const buildAdminRouter = (admin) => {
-  const router = buildAuthenticatedRouter(admin, {
-    cookieName: 'admin-bro',
-    cookiePassword: 'superlongandcomplicatedname',
-    authenticate: async (email, password) => {
-      const company = await Company.findOne({ email });
+  // Remove the authentication logic
+  const router = buildRouter(admin);
+  
+  // Add session handling without authentication
+  router.use(
+    session({
+      resave: false,
+      saveUninitialized: true,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+      secret: 'superlongandcomplicatedname',
+    })
+  );
 
-      if (company && await argon2.verify(company.encryptedPassword, password)) {
-        return company.toJSON();
-      }
-        
-
-
-      if (email === '1' && password === '1') {
-        return {
-          email: '1',
-        };
-      }
-
-      return null;
-    },
-  }, null, {
-    resave: false,
-    saveUninitialized: true,
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  });
   return router;
 };
 
